54af65da503ac74c3a4bce84b4fbb36f55d6aeeb
tanzir ibne ali <tanziribneali@gmail.com>
2023-12-10 15:44:29 +0600

    finish home components

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from './Layout';
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
+            <Layout className='py-8 flex items-center justify-between'>
+                <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+                <div className='flex items-center'>
+               Build With <span className='text-primary text-2xl px-1'>&#9825;</span> by&nbsp;<Link href="/" className='underline underline-offset-2'>TanzirIbneAli</Link>
+                </div>
+                <Link href="/" className='underline underline-offset-2'>Say hello</Link>
+            </Layout>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,96 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+import {
+  DribbbleIcon,
+  GithubIcon,
+  LinkedInIcon,
+  PinterestIcon,
+  TwitterIcon,
+} from "./Icons";
+import { motion } from "framer-motion";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span
+        className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 mx-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <PinterestIcon />
+        </motion.a>
+        <motion.a
+          href="https://twitter.com"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          className="w-6 ml-3"
+          whileTap={{ scale: 0.9 }}
+        >
+          <DribbbleIcon />
+        </motion.a>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
